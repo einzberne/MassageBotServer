c0b5ec34c54c61fb8c7e48aabaaac599a5280ad2
einzberne <einzberne@gmail.com>
2023-07-12 17:23:57 +0700

    add log to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    console.log(req.query)
+    console.log(req.rawHeaders)
+    console.log(req.params)
+    res.send("Express on Vercel");
 });
 app.listen(5000, () => {
-  console.log("Running on port 5000.");
+    console.log("Running on port 5000.");
 });
 // Export the Express API
 module.exports = app;
