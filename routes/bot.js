9e4741d4a9ed8ccf0546b27a813ac8de85970cb0
einzberne <einzberne@gmail.com>
2023-07-12 18:17:41 +0700

    update bot.js

--- routes/bot.js
+++ routes/bot.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
-const logger = require("../winstonLog");
 
 router.post("/", async (req, res) => {
-    logger.info(req.query);
-    logger.info(req.headers);
-    logger.info(req.params);
     console.log(req.query)
     console.log(req.headers)
     console.log(req.params)
