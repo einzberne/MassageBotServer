453dca129754d87bab06f64d58c88899ac8fe173
einzberne <einzberne@gmail.com>
2023-07-12 18:20:20 +0700

    move winstonLog to util

--- routes/bot.js
+++ routes/bot.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
+const logger = require("../utils/winstonLog");
 
 router.post("/", async (req, res) => {
+    logger.info(req.query);
+    logger.info(req.headers);
+    logger.info(req.params);
     console.log(req.query)
     console.log(req.headers)
     console.log(req.params)
