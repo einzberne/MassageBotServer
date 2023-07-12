52b374101c5a73c21a09d131941c456ed2b64cb3
einzberne <einzberne@gmail.com>
2023-07-12 17:36:13 +0700

    add post /bot url

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
+
+app.post("/bot", (req, res) => {
     console.log(req.query)
     console.log(req.rawHeaders)
     console.log(req.params)
-    res.send("Express on Vercel");
+    return res.status(200)
 });
+
 app.listen(5000, () => {
     console.log("Running on port 5000.");
 });
