f383af14f1d991741c30308a359391eab52bbc74
einzberne <einzberne@gmail.com>
2023-07-12 17:21:01 +0700

    update index.js for using with vercel

---
+++ _index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer")
+const path = require("path");
+
+const { json } = require("express/lib/response");
+const cors = require('cors')
+
+app.use(cors());
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+    console.log(req.query)
+    console.log(req.rawHeaders)
+    console.log(req.params)
+    res.status(200).json('app work')
+})
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
+app.listen(PORT, () => {
+    console.log(`Backend Serer is running. ${PORT}`)
+})
+
+// Export the Express API
+module.exports = app;
