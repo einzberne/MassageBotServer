387e10ded209266fe8111c07c2e96f7dabcdb166
einzberne <einzberne@gmail.com>
2023-07-12 18:36:33 +0700

    delete comment log

--- routes/bot.js
+++ routes/bot.js
@@ -1,11 +1,7 @@
 const router = require("express").Router();
-// const logger = require("./winstonLog");
 
 router.post("/", async (req, res) => {
   try {
-    // logger.info(req.query);
-    // logger.info(req.headers);
-    // logger.info(req.params);
     console.log(req.query);
     console.log(req.headers);
     console.log(req.params);
