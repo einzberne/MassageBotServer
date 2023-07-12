7f8f3c6662bc17ee7db5c311e5090e5539ab73df
einzberne <einzberne@gmail.com>
2023-07-12 18:25:35 +0700

    add try to bot.js

--- routes/bot.js
+++ routes/bot.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const logger = require("../utils/winstonLog");
 
 router.post("/", async (req, res) => {
+  try {
     logger.info(req.query);
     logger.info(req.headers);
     logger.info(req.params);
-    console.log(req.query)
-    console.log(req.headers)
-    console.log(req.params)
+    console.log(req.query);
+    console.log(req.headers);
+    console.log(req.params);
     res.status(200).json();
+  } catch (err) { res.status(500).json(err)}
 });
 
 module.exports = router;
