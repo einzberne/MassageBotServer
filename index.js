6b77efe9120f89d9b5e969e54be7797c02269aae
einzberne <einzberne@gmail.com>
2023-07-13 12:59:32 +0700

    add log for source and message

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
 
-app.post("/bot", (req, res) => {
-    console.log(req.query)
-    console.log(req.rawHeaders)
-    console.log(req.params)
-    return res.status(200)
-});
-
 app.listen(5000, () => {
     console.log("Running on port 5000.");
 });
