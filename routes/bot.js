49be446ef168a4715e077ad056f041719a9c38b6
einzberne <einzberne@gmail.com>
2023-07-13 18:47:11 +0700

    add log

--- routes/bot.js
+++ routes/bot.js
@@ -6,9 +6,9 @@ router.post("/", async (req, res) => {
     console.log(req.query);
     console.log(req.headers);
     console.log(req.body);
-    console.log(req.body.source);
-    console.log(req.body.message);
-    console.log(req.body.deliveryContext);
+    console.log(req.body.events[0].source);
+    console.log(req.body.events[0].message);
+    console.log(req.body.events[0].deliveryContext);
     res.status(200).json();
   } catch (err) { res.status(500).json(err)}
 });
